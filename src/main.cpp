23be188154576b6d3941f38a156e92edebf62dad
Vova Abdrakhmanov <369565@gmail.com>
2017-04-01 10:32:50 +0300

    [fix] GLFW_TRUE not work in Ubuntu 16.04

--- src/main.cpp
+++ src/main.cpp
@@ -50,7 +50,7 @@ void error_callback(int error, const char* description) {
 
 void key_callback(GLFWwindow * window, int key, int scancode, int action, int mods) {
     if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS) {
-        glfwSetWindowShouldClose(window, GLFW_TRUE);
+        glfwSetWindowShouldClose(window, true);
     }
     if (key == GLFW_KEY_SPACE && action == GLFW_PRESS) {
         pause_flag = !pause_flag;
